477c50629aecd9c8cdd3272370b050a2b6592d71
Christoffer Gretarsson Alexandersen <christoffer.alexandersen@maths.ox.ac.uk>
2023-10-23 13:48:45 +0100

    suppressing some jitcdde warnings

--- brain_models.py
+++ brain_models.py
@@ -9,6 +9,7 @@ from numba import jit
 from math import pi
 import warnings
 warnings.filterwarnings("ignore", message="Differential equation does not include a delay term.")
+warnings.filterwarnings("ignore", message="The target time is smaller than the current time. No integration step will happen. The returned state will be extrapolated from the interpolating Hermite polynomial for the last integration step. You may see this because you try to integrate backwards in time, in which case you did something wrong. You may see this just because your sampling step is small, in which case there is no need to worry.")
 
 # -----------------------------------------------
 # in this module, we include functions to compile
